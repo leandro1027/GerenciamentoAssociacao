aaeaa39a41ba318fab5cc9c6f7b5ba7e2f0db263
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-07-14 10:52:00 -0300

    Create textarea.tsx

---
+++ Frontend/app/components/common/textarea.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type TextareaProps = React.ComponentProps<'textarea'>;
+
+const Textarea = (props: TextareaProps) => {
+  return (
+    <textarea
+      {...props}
+      className={`w-full px-4 py-3 bg-gray-100 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 focus:bg-white transition-colors duration-200 ${props.className}`}
+      rows={4} // Define uma altura padrão
+    />
+  );
+};
+
+export default Textarea;
