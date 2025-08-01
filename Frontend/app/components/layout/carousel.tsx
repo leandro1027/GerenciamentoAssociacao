79855d3ce4e34a9bce2273f7b58569fd7a6c1304
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-08-01 12:35:01 -0300

    feat(ui): carrousel slides da home page responsivos para a visualização mobile

--- Frontend/app/components/layout/carousel.tsx
+++ Frontend/app/components/layout/carousel.tsx
@@ -10,7 +10,6 @@ const SlideGallery = () => {
   useEffect(() => {
     api.get<Slide[]>('/slide')
       .then(response => {
-        // Limita a quantidade de slides
         setSlides(response.data.slice(0, 2));
       })
       .catch(error => console.error("[SlideGallery] ERRO ao buscar slides:", error));
@@ -23,12 +22,10 @@ const SlideGallery = () => {
   const apiBaseUrl = api.defaults.baseURL;
 
   return (
-
     <div className="w-full bg-white">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
         {slides.map((slide) => (
-          // Altura fixa aqui para controlar o tamanho da imagem
-          <div key={slide.id} className="overflow-hidden group h-80"> {/* Altura de 320px */}
+          <div key={slide.id} className="overflow-hidden group h-40 sm:h-80">
             <img 
               src={`${apiBaseUrl}${slide.imageUrl}`} 
               alt={slide.title} 
