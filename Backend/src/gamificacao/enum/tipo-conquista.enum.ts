36612624be6aa555ce18df3d990ad5b4e71ea5f1
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-09-21 22:45:24 -0300

    Create tipo-conquista.enum.ts

---
+++ Backend/src/gamificacao/enum/tipo-conquista.enum.ts
@@ -0,0 +1,7 @@
+export enum TipoConquista {
+  PRIMEIRO_APOIADOR = 'Primeiro Apoiador',
+  HEROI_DOS_PELUDOS = 'Herói dos Peludos',
+  ANJO_DA_GUARDA = 'Anjo da Guarda',
+  VOZ_DOS_SEM_VOZ = 'Voz dos Sem Voz',
+}
+
