5eaccf6f0b064ea8701d5bac4dfe2846bb6ce136
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-10-04 14:47:56 -0300

    .

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import Link from 'next/link';
 import Carousel from './components/layout/carousel';
 import api from './services/api';
