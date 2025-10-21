348c5ac84068a5225c1a41fa4b8f18550c7083b7
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-10-20 23:04:51 -0300

    Update api.ts

--- Frontend/app/services/api.ts
+++ Frontend/app/services/api.ts
@@ -6,4 +6,6 @@ const api = axios.create({
   
 });
 
+export const CLOUDFLARE_R2_URL = process.env.NEXT_PUBLIC_CLOUDFLARE_R2_URL || 'https://pub-c5a813e903524beb8500fe1fd3de9efe.r2.dev/';
+
 export default api;
